0abd3915e515180593bcacec345eda8f30096adb
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-05-18 14:51:11 -0300

    update CreateUserUseCaseTest with password validations

--- redline-core/src/test/java/com/yuriolivs/redlinecore/usecase/user/CreateUserUseCaseTest.java
+++ redline-core/src/test/java/com/yuriolivs/redlinecore/usecase/user/CreateUserUseCaseTest.java
@@ -1,10 +1,10 @@
 package com.yuriolivs.redlinecore.usecase.user;
 
 import com.yuriolivs.redlinecore.domain.repository.UserRepositoryInterface;
+import com.yuriolivs.redlinecore.domain.security.PasswordEncrypter;
 import com.yuriolivs.redlinecore.domain.user.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.time.LocalDate;
@@ -14,12 +14,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CreateUserUseCaseTest {
     private UserRepositoryInterface userRepository;
+    private PasswordEncrypter encrypter;
     private CreateUserUseCase useCase;
 
     @BeforeEach
     void setUp() {
         userRepository = Mockito.mock(UserRepositoryInterface.class);
-        useCase = new CreateUserUseCase(userRepository);
+        encrypter = Mockito.mock(PasswordEncrypter.class);
+        useCase = new CreateUserUseCase(userRepository, encrypter); //ajustar essa parte implementando o password encrypter
     }
 
     @Test
@@ -32,24 +34,32 @@ public class CreateUserUseCaseTest {
                 LocalDate.now().minusYears(20)
         );
 
-        Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(user);
+        Mockito.when(userRepository.save(Mockito.any(User.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        Mockito.when(encrypter.encrypt(Mockito.any(String.class)))
+                .thenReturn(String.join("_", "hashed", user.getPassword()));
 
         User userCreated = useCase.execute(
-                "Yuri",
-                "Oliveira",
-                "yuri@email.com",
-                "Senha@123456",
-                LocalDate.now().minusYears(20)
+                user.getName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getDateOfBirth()
         );
 
         assertNotNull(userCreated);
         assertEquals("Yuri", userCreated.getName());
+        assertEquals(
+                String.join("_", "hashed", user.getPassword()),
+                userCreated.getPassword()
+        );
 
         Mockito.verify(userRepository).save(Mockito.any(User.class));
     }
 
     @Test
-    void shouldThrowExceptionWhenEmailAlreadyExists() {
+    void assertThrowsExceptionWhenEmailAlreadyExists() {
         Mockito.when(userRepository.findByEmail("yuri@email.com")).thenReturn(Optional.of(Mockito.mock(User.class)));
 
         assertThrows(IllegalArgumentException.class, () ->
@@ -63,5 +73,158 @@ public class CreateUserUseCaseTest {
         );
 
         Mockito.verify(userRepository, Mockito.never()).save(Mockito.any());
+    }
+
+    @Test
+    void assertThrowsExceptionWhenPasswordIsTooShort() {
+        User user = new User(
+                "Yuri",
+                "Oliveira",
+                "yuri@email.com",
+                "Sen",
+                LocalDate.now().minusYears(20)
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            useCase.execute(
+                    user.getName(),
+                    user.getLastName(),
+                    user.getEmail(),
+                    user.getPassword(),
+                    user.getDateOfBirth()
+            );
+        });
+    }
+
+    @Test
+    void assertThrowsExceptionWhenPasswordIsTooBig() {
+        User user = new User(
+                "Yuri",
+                "Oliveira",
+                "yuri@email.com",
+                "Senha@123456ggsdfgsdfgsdfgdgzdfgdg",
+                LocalDate.now().minusYears(20)
+        );
+
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            useCase.execute(
+                    user.getName(),
+                    user.getLastName(),
+                    user.getEmail(),
+                    user.getPassword(),
+                    user.getDateOfBirth()
+            );
+        });
+    }
+
+    @Test
+    void assertThrowsExceptionWhenPasswordDoesNotHaveLetters() {
+        User user = new User(
+                "Yuri",
+                "Oliveira",
+                "yuri@email.com",
+                "@12345678910",
+                LocalDate.now().minusYears(20)
+        );
+
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            useCase.execute(
+                    user.getName(),
+                    user.getLastName(),
+                    user.getEmail(),
+                    user.getPassword(),
+                    user.getDateOfBirth()
+            );
+        });
+    }
+
+    @Test
+    void assertThrowsExceptionWhenPasswordDoesNotHaveNumbers() {
+        User user = new User(
+                "Yuri",
+                "Oliveira",
+                "yuri@email.com",
+                "Senha@Senha!",
+                LocalDate.now().minusYears(20)
+        );
+
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            useCase.execute(
+                    user.getName(),
+                    user.getLastName(),
+                    user.getEmail(),
+                    user.getPassword(),
+                    user.getDateOfBirth()
+            );
+        });
+    }
+
+    @Test
+    void assertThrowsExceptionWhenPasswordDoesNotHaveSpecialChars() {
+        User user = new User(
+                "Yuri",
+                "Oliveira",
+                "yuri@email.com",
+                "Senha1234567",
+                LocalDate.now().minusYears(20)
+        );
+
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            useCase.execute(
+                    user.getName(),
+                    user.getLastName(),
+                    user.getEmail(),
+                    user.getPassword(),
+                    user.getDateOfBirth()
+            );
+        });
+    }
+
+    @Test
+    void assertThrowsExceptionWhenPasswordDoesNotHaveUppercaseLetters() {
+        User user = new User(
+                "Yuri",
+                "Oliveira",
+                "yuri@email.com",
+                "senha@123456",
+                LocalDate.now().minusYears(20)
+        );
+
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            useCase.execute(
+                    user.getName(),
+                    user.getLastName(),
+                    user.getEmail(),
+                    user.getPassword(),
+                    user.getDateOfBirth()
+            );
+        });
+    }
+
+    @Test
+    void assertThrowsExceptionWhenPasswordDoesNotHaveLowercaseLetters() {
+        User user = new User(
+                "Yuri",
+                "Oliveira",
+                "yuri@email.com",
+                "SENHA@123456",
+                LocalDate.now().minusYears(20)
+        );
+
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            useCase.execute(
+                    user.getName(),
+                    user.getLastName(),
+                    user.getEmail(),
+                    user.getPassword(),
+                    user.getDateOfBirth()
+            );
+        });
     }
 }
