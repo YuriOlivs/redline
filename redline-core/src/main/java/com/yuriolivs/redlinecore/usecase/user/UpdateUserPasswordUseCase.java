fccfb086e5f38dd85d6a116b6b8bf61261a33d04
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-05-19 16:48:13 -0300

    add UpdateUserPassword basic code (not implemented yet)

--- redline-core/src/main/java/com/yuriolivs/redlinecore/usecase/user/UpdateUserPasswordUseCase.java
+++ redline-core/src/main/java/com/yuriolivs/redlinecore/usecase/user/UpdateUserPasswordUseCase.java
@@ -1,4 +1,23 @@
 package com.yuriolivs.redlinecore.usecase.user;
 
+import com.yuriolivs.redlinecore.domain.repository.UserRepositoryInterface;
+import com.yuriolivs.redlinecore.domain.security.PasswordEncrypter;
+import com.yuriolivs.redlinecore.domain.user.User;
+
 public class UpdateUserPasswordUseCase {
+    private final UserRepositoryInterface userRepository;
+    private final PasswordEncrypter encrypter;
+
+    public UpdateUserPasswordUseCase(UserRepositoryInterface userRepository, PasswordEncrypter encrypter) {
+        this.userRepository = userRepository;
+        this.encrypter = encrypter;
+    }
+
+    public boolean execute(
+            User user,
+            String oldPassword,
+            String newPassword
+    ) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
 }
