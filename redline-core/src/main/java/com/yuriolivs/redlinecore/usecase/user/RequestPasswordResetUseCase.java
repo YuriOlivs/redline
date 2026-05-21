379645b22d740e672ec06e557350ba744df72026
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-05-21 01:45:15 -0300

    feat: add and implement RequestPasswordResetUseCaseTest and add cacheService to its original use case

--- redline-core/src/main/java/com/yuriolivs/redlinecore/usecase/user/RequestPasswordResetUseCase.java
+++ redline-core/src/main/java/com/yuriolivs/redlinecore/usecase/user/RequestPasswordResetUseCase.java
@@ -1,6 +1,7 @@
 package com.yuriolivs.redlinecore.usecase.user;
 
 import com.yuriolivs.redlinecore.domain.repository.UserRepositoryInterface;
+import com.yuriolivs.redlinecore.domain.service.CacheServiceInterface;
 import com.yuriolivs.redlinecore.domain.service.CodeGeneratorInterface;
 import com.yuriolivs.redlinecore.domain.service.EmailSenderInterface;
 
@@ -8,18 +9,21 @@ public class RequestPasswordResetUseCase {
     private final UserRepositoryInterface userRepository;
     private final EmailSenderInterface emailSender;
     private final CodeGeneratorInterface codeGenerator;
+    private final CacheServiceInterface cacheService;
 
     public RequestPasswordResetUseCase(
             UserRepositoryInterface userRepository,
             EmailSenderInterface emailSender,
-            CodeGeneratorInterface codeGenerator
+            CodeGeneratorInterface codeGenerator,
+            CacheServiceInterface cacheService
     ) {
         this.userRepository = userRepository;
         this.emailSender = emailSender;
         this.codeGenerator = codeGenerator;
+        this.cacheService = cacheService;
     }
 
-    void execute(String email) {
+    String execute(String email) {
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 }
