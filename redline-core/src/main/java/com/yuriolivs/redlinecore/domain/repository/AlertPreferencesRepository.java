0aafbd45eff796cbe2fe731a4fc239391c3e467b
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-06-18 21:02:45 -0300

    feat: implement update alert preferences use case and update it's repository

--- redline-core/src/main/java/com/yuriolivs/redlinecore/domain/repository/AlertPreferencesRepository.java
+++ redline-core/src/main/java/com/yuriolivs/redlinecore/domain/repository/AlertPreferencesRepository.java
@@ -2,6 +2,9 @@ package com.yuriolivs.redlinecore.domain.repository;
 
 import com.yuriolivs.redlinecore.domain.alert.AlertPreferences;
 
+import java.util.UUID;
+
 public interface AlertPreferencesRepository {
     AlertPreferences save(AlertPreferences preferences);
+    AlertPreferences findByUser(UUID userId);
 }
