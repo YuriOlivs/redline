5e8ce84b09d682da022093d00280d92eed09cf63
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-07-20 16:41:15 -0300

    feat: add list validation to AdsScrapedEvent

--- redline-core/src/main/java/com/yuriolivs/redlinecore/domain/event/AdsScrapedEvent.java
+++ redline-core/src/main/java/com/yuriolivs/redlinecore/domain/event/AdsScrapedEvent.java
@@ -6,4 +6,14 @@ import java.util.List;
 
 public record AdsScrapedEvent(
         List<Advertisement> advertisements
-) {}
+) {
+
+    public AdsScrapedEvent{
+        validateAdvertisements(advertisements);
+    }
+
+    private void validateAdvertisements(List<Advertisement> advertisement) {
+        if (advertisement == null || advertisement.isEmpty())
+            throw new IllegalArgumentException("Advertisement list can't be null or empty");
+    }
+}
