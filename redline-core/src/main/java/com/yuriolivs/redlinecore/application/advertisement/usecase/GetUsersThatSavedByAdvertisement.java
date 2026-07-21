61d42301f47766b87cb6fde01d74451efb215cca
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-07-21 09:38:55 -0300

    feat: add GetUsersThatSavedByAdvertisement use case

---
+++ redline-core/src/main/java/com/yuriolivs/redlinecore/application/advertisement/usecase/GetUsersThatSavedByAdvertisement.java
@@ -0,0 +1,30 @@
+package com.yuriolivs.redlinecore.application.advertisement.usecase;
+
+import com.yuriolivs.redlinecore.domain.advertisement.Advertisement;
+import com.yuriolivs.redlinecore.domain.exceptions.NotFoundException;
+import com.yuriolivs.redlinecore.domain.repository.IAdvertisementRepository;
+import com.yuriolivs.redlinecore.domain.repository.ISavedAdvertisementRepository;
+import com.yuriolivs.redlinecore.domain.user.User;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class GetUsersThatSavedByAdvertisement {
+    private final ISavedAdvertisementRepository savedAdvertisementRepository;
+    private final IAdvertisementRepository advertisementRepository;
+
+    public List<User> execute(String url) {
+        if (url == null || !url.isEmpty()) {
+            throw new IllegalArgumentException("Advertisement can't be null or inactive");
+        }
+
+        Optional<Advertisement> adFound = advertisementRepository.findByUrl(url);
+        if (adFound.isEmpty()) {
+            throw new NotFoundException("Advertisement");
+        }
+
+        return savedAdvertisementRepository.findUsersByAdvertisement(url);
+    }
+}
