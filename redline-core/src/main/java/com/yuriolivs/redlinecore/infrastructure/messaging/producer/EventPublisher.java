8d0a4a5e9fbabe4a804a966f4626d2b71e4837f0
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-07-23 19:09:46 -0300

    feat: implement global event publisher

--- redline-core/src/main/java/com/yuriolivs/redlinecore/infrastructure/messaging/producer/EventPublisher.java
+++ redline-core/src/main/java/com/yuriolivs/redlinecore/infrastructure/messaging/producer/EventPublisher.java
@@ -5,20 +5,33 @@ import com.yuriolivs.redlinecore.domain.event.AdUpdatedEvent;
 import com.yuriolivs.redlinecore.domain.event.AdsScrapedEvent;
 import com.yuriolivs.redlinecore.domain.event.DomainEvent;
 import com.yuriolivs.redlinecore.domain.service.IEventPublisher;
+import com.yuriolivs.redlinecore.infrastructure.messaging.config.RabbitMqConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class EventPublisher implements IEventPublisher {
+    private final RabbitTemplate rabbitTemplate;
+
     @Override
     public void publish(DomainEvent event) {
-        if (event instanceof AdUpdatedEvent) {
+        rabbitTemplate.convertAndSend(
+                RabbitMqConfig.EXCHANGE,
+                defineRoutingKey(event),
+                event
+        );
+    }
 
-        }
+    private String defineRoutingKey(DomainEvent event) {
+        if (event instanceof AdsScrapedEvent)
+            return RabbitMqConfig.ADS_SCRAPED_ROUTING_KEY;
+        if (event instanceof AdUpdatedEvent)
+            return RabbitMqConfig.AD_UPDATED_ROUTING_KEY;
+        if (event instanceof AdAlertTriggeredEvent)
+            return RabbitMqConfig.ALERT_ROUTING_KEY;
 
-        if (event instanceof AdsScrapedEvent) {
-
-        }
-
-        if (event instanceof AdAlertTriggeredEvent) {
-
-        }
+        throw new IllegalArgumentException("No routing key found for "+ event.getClass());
     }
 }
